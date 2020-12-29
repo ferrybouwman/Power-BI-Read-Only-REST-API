bfcd0bcb67f9384f757e61d1f1d90c2fdaf36982
Ferry Bouwman <ferrybouwman@gmail.com>
2020-12-29 14:42:24 +0100

    Delete trigger_from_powershell.ps1

--- Power Automate (Flow)/Examples for using this Flow's output/trigger_from_powershell.ps1
+++
@@ -1,8 +0,0 @@
-$modifiedSince = "2020-12-29T13:02:38.3525654Z" # 24 hour time notation
-
-$response = Invoke-WebRequest `
-    -Uri "past HTTP GET URL from FLOW" `
-    -Method Get `
-    -ContentType application/json
-
-# HTTP GET URL should look something like: https://prod-28.westeurope.logic.azure.com/workflows/c49fda0c49fda0c49fda8688a46da0c4/triggers/manual/paths/invoke/modifiedSince/$($modifiedSince)?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=JyX4ySR5_RltZcgM4WEs
